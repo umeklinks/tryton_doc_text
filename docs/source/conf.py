3c4b7111cd1ebea2a4f026b9c8827a437c08086e
Ume Abraham Kalu <umekalu@gmail.com>
2020-05-19 17:23:19 +0100

    Opened sphinx_copybutton
    To check for error correction

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'sphinx_copybutton'
+    'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
